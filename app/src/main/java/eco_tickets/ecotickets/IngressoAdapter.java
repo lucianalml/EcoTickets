0b90d1c768552af84633a4dbee12b1ee6d55f5c1
lucianalml <limao.luciana@gmail.com>
2016-04-14 22:44:12 -0300

    Mais legivel

--- app/src/main/java/eco_tickets/ecotickets/IngressoAdapter.java
+++ app/src/main/java/eco_tickets/ecotickets/IngressoAdapter.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CheckBox;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -62,6 +63,7 @@ public class IngressoAdapter extends RecyclerView.Adapter<IngressoAdapter.ViewHo
 
                 notifyItemChanged(clickedPos);
 
+                Toast.makeText(v.getContext(), "Check-in atualizado.", Toast.LENGTH_SHORT).show();
             }
         });
 
