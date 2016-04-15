53e216445348ec4ba3b5744e63bca6fc4654d92f
lucianalml <limao.luciana@gmail.com>
2016-04-15 00:17:31 -0300

    Menu de configuração

--- app/src/main/java/eco_tickets/ecotickets/MainActivity.java
+++ app/src/main/java/eco_tickets/ecotickets/MainActivity.java
@@ -1,8 +1,9 @@
 package eco_tickets.ecotickets;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
 
         setUI();
         setActions();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 
     private void setUI(){
@@ -46,6 +49,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
 
